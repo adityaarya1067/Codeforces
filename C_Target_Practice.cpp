370e71f0dab5c819d47ce7f54ac3541f3e60bd29
Aditya Kumar Arya <aryaadityakumar222@gmail.com>
2025-01-22 19:20:58 +0530

    initial

---
+++ C_Target_Practice.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int RING_VALUES[10][10] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+    {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+    {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        vector<string> grid(10);
+        for (int i = 0; i < 10; i++) {
+            cin >> grid[i];
+        }
+
+        int total_score = 0;
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (grid[i][j] == 'X') {
+                    total_score += RING_VALUES[i][j];
+                }
+            }
+        }
+
+        cout << total_score << endl;
+    }
+    return 0;
+}
